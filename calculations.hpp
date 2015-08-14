2c7bb18a09c49becf42bc0c14fb20ad0b5999d4a
Emily Puth <emily.puth@gmail.com>
2015-08-14 13:34:10 -0700

    Create calculations.hpp

---
+++ calculations.hpp
@@ -0,0 +1,21 @@
+//
+//
+// Name: Emily Puth
+// ID: 28239807
+//
+//
+
+#ifndef CALCULATIONS_HPP_
+#define CALCULATIONS_HPP_
+
+#include <string>
+#include <iostream>
+#include <math.h>
+#include "roof-and-fence-constants.hpp"
+
+
+int calculateNeedQuantity(int lengthOrCover, float userWants);
+void fencePrompt(int l,int p);
+
+
+#endif /* CALCULATIONS_HPP_ */
